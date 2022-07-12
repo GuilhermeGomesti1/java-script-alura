4038bd729d3ed874ad5fcf9c12640e388ab468c5
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2022-07-12 20:48:11 -0300

    js

---
+++ introducao-javascript/js/principal.js
@@ -0,0 +1,14 @@
+let titulo = document.querySelector("#titulo");			
+titulo.textContent = "Aparecida Nutricionistas";
+
+let paciente = document.querySelector("#primeiro-paciente");
+
+let tdPeso = paciente.querySelector(".info-peso");
+
+let peso = tdPeso.textContent;
+let tdAltura = paciente.querySelector('.info-altura');
+let altura = tdAltura.textContent;
+
+
+let imc = peso / (altura * altura);
+console.log(imc)
