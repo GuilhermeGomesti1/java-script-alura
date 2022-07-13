9a471ba0a664fb4e0a58a9982f36a977e80beee9
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2022-07-13 17:21:44 -0300

    att alura

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -2,9 +2,13 @@
 let titulo = document.querySelector("#titulo");			
 titulo.textContent = "Aparecida Nutricionistas";
 
-let paciente = document.querySelector("#primeiro-paciente");
+let pacientes = document.querySelectorAll(".paciente");
 
-let tdPeso = paciente.querySelector(".info-peso");
+
+for(let i = 0; i < pacientes.length; i++){
+   var paciente = pacientes[i];
+
+   let tdPeso = paciente.querySelector('.info-peso');
 
 let peso = tdPeso.textContent;
 let tdAltura = paciente.querySelector('.info-altura');
@@ -12,36 +16,42 @@ let altura = tdAltura.textContent;
 
 let tdImc = paciente.querySelector(".info-imc");
 
-let imc = peso / (altura * altura);
 
-tdImc.textContent = imc;
+let pesoEhValido = true;
+let alturaEhValida = true;
 
-document.querySelector('form').addEventListener('submit',(e)=>{
-    e.preventDefault();
-    const nome = document.querySelector('#nome')
-    const peso = document.querySelector('#peso')
-    const altura = document.querySelector('#altura')
-    const gordura = document.querySelector('#gordura')
 
-    const tabela = document.querySelector("#tabela-pacientes")
-
-    const el = document.createElement('tr')
-    const n = document.createElement('td')
-    n.className='info-nome'
-    n.textContent=nome.value
-    el.appendChild(n)
-    tabela.appendChild(el)
+if(peso <= 0 || peso > 300) {
+    console.log('peso inválido')
+    pesoEhvalido = false;
+    tdImc.textContent = 'Peso inválido!';
+    tdImc.className = 'invalido'
     
+}
+
+if(altura < 0 || altura > 3) {
+    console.log('altura inválida')
+    alturaEhValida= false;
+    tdImc.textContent = 'Altura inválido!';
+    tdImc.className = 'invalido'
+    
+}
+
+
+if (alturaEhValida && pesoEhValido)
+ {
+let imc = peso / (altura * altura);
+tdImc.textContent = imc.toFixed(2);}
+else {
+    tdImc.textContent = 'altura e peso inválidos'
+   tdImc.className = 'invalido'
+   paciente.style.backgroundColor = "lightcoral";
+}
 
 
 
-})
+
+}
 
 
-// <tr class="paciente">
-// 						<td class="info-nome">João</td>
-// 						<td class="info-peso">80</td>
-// 						<td class="info-altura">1.72</td>
-// 						<td class="info-gordura">40</td>
-// 						<td class="info-imc">0</td>
-// 					</tr>
+
