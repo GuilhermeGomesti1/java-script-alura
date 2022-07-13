1cc4b3a62298963e3184c7ef31f1573d91e2a259
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2022-07-13 15:01:14 -0300

    alura

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,3 +1,4 @@
+
 let titulo = document.querySelector("#titulo");			
 titulo.textContent = "Aparecida Nutricionistas";
 
@@ -9,6 +10,38 @@ let peso = tdPeso.textContent;
 let tdAltura = paciente.querySelector('.info-altura');
 let altura = tdAltura.textContent;
 
+let tdImc = paciente.querySelector(".info-imc");
 
 let imc = peso / (altura * altura);
-console.log(imc)
+
+tdImc.textContent = imc;
+
+document.querySelector('form').addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const nome = document.querySelector('#nome')
+    const peso = document.querySelector('#peso')
+    const altura = document.querySelector('#altura')
+    const gordura = document.querySelector('#gordura')
+
+    const tabela = document.querySelector("#tabela-pacientes")
+
+    const el = document.createElement('tr')
+    const n = document.createElement('td')
+    n.className='info-nome'
+    n.textContent=nome.value
+    el.appendChild(n)
+    tabela.appendChild(el)
+    
+
+
+
+})
+
+
+// <tr class="paciente">
+// 						<td class="info-nome">João</td>
+// 						<td class="info-peso">80</td>
+// 						<td class="info-altura">1.72</td>
+// 						<td class="info-gordura">40</td>
+// 						<td class="info-imc">0</td>
+// 					</tr>
